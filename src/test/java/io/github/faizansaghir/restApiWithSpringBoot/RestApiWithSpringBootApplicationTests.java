94e96fad212d1350b01a00fc009e538fe4237cbf
faizansaghir <dbz.rashidsaghir@gmail.com>
2024-11-10 23:09:04 +0530

    Scratch code for repository

---
+++ src/test/java/io/github/faizansaghir/restApiWithSpringBoot/RestApiWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.faizansaghir.restApiWithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
